1820140785556325361fbd308793592f5d667917
Rachelle Mitchell <rachelle-mitchell@lambdastudents.com>
2020-10-03 23:01:50 -0500

    complete task 1

--- index.js
+++ index.js
@@ -12,6 +12,12 @@ console.log('its working');
 (d) Away Team goals for 2014 world cup final
 (e) Winner of 2014 world cup final */
 
+const worldCupFinal2014 = fifaData.find(game => game.Year === 2014 && game.Stage === "Final");
+console.log(worldCupFinal2014["Home Team Name"]);
+console.log(worldCupFinal2014["Away Team Name"]);
+console.log(worldCupFinal2014["Home Team Goals"]);
+console.log(worldCupFinal2014["Away Team Goals"]);
+
 
 /* Task 2: Create a function called  getFinals that takes `data` as an argument and returns an array of objects with only finals data */
 
